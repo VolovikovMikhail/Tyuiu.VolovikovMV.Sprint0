d20ddd3d53ec1a0cfe1c452896fe381421e6e48b
volov <volov@DESKTOP-N59KU5K>
2023-09-06 15:10:54 +0500

    Add project files.

---
+++ Tyuiu.VolovikovMV.Sprint0.Taks0.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VolovikovMV.Sprint0.Taks0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
