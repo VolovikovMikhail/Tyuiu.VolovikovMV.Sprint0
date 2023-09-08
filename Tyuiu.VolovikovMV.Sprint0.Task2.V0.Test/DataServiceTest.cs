790b813cb1379bc23c25e70e94897f121dfae272
VolovikovMikhail <volovikov.1995@bk.ru>
2023-09-08 20:17:21 +0500

    Task2

---
+++ Tyuiu.VolovikovMV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VolovikovMV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Михаил";
+            var res = DataService.GetMessage(name);
+            
+            Assert.AreEqual("Привет, Михаил", res);
+        }
+    }
+}
