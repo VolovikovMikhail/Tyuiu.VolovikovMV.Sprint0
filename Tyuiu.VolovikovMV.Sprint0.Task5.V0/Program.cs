b514af6ce2bf76cdd027db6cc796f71a5f520d1d
VolovikovMikhail <volovikov.1995@bk.ru>
2023-09-15 15:11:47 +0500

    Task5

---
+++ Tyuiu.VolovikovMV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolovikovMV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.VolovikovMV.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a + b = " + DataService.Addition(1, 5));
+            Console.WriteLine("a - b = " + DataService.Substraction(15, 5));
+            Console.WriteLine("a * b = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("a / b = " + DataService.Division(9, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
